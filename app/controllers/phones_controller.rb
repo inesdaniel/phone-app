ccff03c258567b06cbe007168811877b3f54a70a
Ines Daniel <i.danie8@gmail.com>
2018-08-22 18:45:48 -0500

    add crud actions

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,4 +1,39 @@
 class PhonesController < ApplicationController
   def index
+    @phones = Phone.all
+    render 'index.html.erb'
+  end
+  def show
+    @phone = Phone.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
+  def new
+    render 'new.html.erb'
+  end
+  def create
+    phone = Phone.new(
+      color: params[:color] ,
+      brand: params[:brand] ,
+      size: params[:size]
+    )
+    phone.save
+    redirect_to "/phones/#{phone.id}"
+  end
+  def edit
+    @phone = Phone.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+  def update
+    phone = Phone.find_by(id: params[:id])
+    phone.color = params[:color]
+    phone.brand = params[:brand]
+    phone.size = params[:size]
+    phone.save
+    redirect_to "/phones/#{phone.id}"
+  end
+  def destroy
+    phone = Phone.find_by(id: params[:id])
+    phone.destroy
+    redirect_to "/phones"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  get '/phones' => 'phones#index'
+  get '/phones/new' => 'phones#new'
+  get '/phones/:id' => 'phones#show'
+  get '/phones/:id/edit' => 'phones#edit'
+  patch '/phones/:id' => 'phones#update'
+  post '/phones' => 'phones#create'
+  delete '/phones/:id' => 'phones#destroy'
 end
