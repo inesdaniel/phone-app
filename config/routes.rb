7adefaf84bfe654dc3945c35128aaa9f816b99bd
Ines Daniel <i.danie8@gmail.com>
2018-08-22 18:52:25 -0500

    change routes file to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
-  get '/phones' => 'phones#index'
-  get '/phones/new' => 'phones#new'
-  get '/phones/:id' => 'phones#show'
-  get '/phones/:id/edit' => 'phones#edit'
-  patch '/phones/:id' => 'phones#update'
-  post '/phones' => 'phones#create'
-  delete '/phones/:id' => 'phones#destroy'
+
+  resources :phones
+
+  # get '/phones' => 'phones#index'
+  # get '/phones/new' => 'phones#new'
+  # get '/phones/:id' => 'phones#show'
+  # get '/phones/:id/edit' => 'phones#edit'
+  # patch '/phones/:id' => 'phones#update'
+  # post '/phones' => 'phones#create'
+  # delete '/phones/:id' => 'phones#destroy'
 end
